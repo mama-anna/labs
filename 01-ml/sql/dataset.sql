033f0eaa5ea1ed26213e39a1a86d7d4ef0998739
Mama Anna <nomail@yandex.ru>
2026-03-11 00:25:58 +0300

    ---

---
+++ 01-ml/sql/dataset.sql
@@ -0,0 +1,25 @@
+create table dataset(
+id INTEGER
+,player_name TEXT
+,team_abbreviation TEXT
+,age REAL
+,player_height REAL
+,player_weight REAL
+,college TEXT
+,country TEXT
+,draft_year INTEGER
+,draft_round REAL
+,draft_number REAL
+,gp REAL
+,pts REAL
+,reb REAL
+,ast REAL
+,net_rating REAL
+,oreb_pct REAL
+,dreb_pct REAL
+,usg_pct REAL
+,ts_pct REAL
+,ast_pct REAL
+,season TEXT
+);
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# labs
+Anna labs
+
